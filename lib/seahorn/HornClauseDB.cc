30644ae07eb3406c67dbc8b350949dfaad505be8
Joachim Bard <joachim.bard@cispa.de>
2022-07-21 19:42:16 +0200

    Change model of fences
    Initially they return false instead of a nondeterministic value

--- lib/seahorn/HornClauseDB.cc
+++ lib/seahorn/HornClauseDB.cc
@@ -173,12 +173,16 @@ raw_ostream &HornClauseDB::write(raw_ostream &o) const {
 }
 
 bool HornClauseDB::changeFenceRules(std::string &name, Expr &newRule) {
+  bool changed = false;
+  std::vector<HornRule> rulesToRemove;
+  std::vector<HornRule> rulesToAdd;
   for (HornRule &rule : m_rules) {
     Expr head = rule.head();
     Expr headDecl = bind::fname(head);
     std::string headName =
         boost::lexical_cast<std::string>(*bind::fname(headDecl));
-    if (name.compare(headName) == 0 && !isOpX<TRUE>(rule.body())) {
+    if (name.compare(headName) == 0) { // && !isOpX<TRUE>(rule.body())) {
+      changed = true;
       outs() << "remove rule: " << *rule.get() << '\n';
 //      outs() << "head args: ";
 //      for (auto it = head->args_begin(); it != head->args_end(); ++it) {
@@ -192,8 +196,8 @@ bool HornClauseDB::changeFenceRules(std::string &name, Expr &newRule) {
 //      }
 //      outs() << '\n';
       ExprVector vars = rule.vars();
-      vars.pop_back();
-      removeRule(rule);
+//      vars.pop_back();
+      rulesToRemove.push_back(rule);
 
       auto firstArg = ++head->args_begin();
       auto lastArg = --head->args_end();
@@ -204,16 +208,17 @@ bool HornClauseDB::changeFenceRules(std::string &name, Expr &newRule) {
 //        newRule = head;
       newRule = bind::fapp(headDecl, args);
       outs() << "add rule: " << *newRule << '\n';
+      rulesToAdd.push_back(HornRule(vars, newRule));
 //      outs() << "new vars: ";
 //      for (Expr e : vars) {
 //        outs() << *e << ", ";
 //      }
 //      outs() << '\n';
-      addRule(vars, newRule);
-      return true;
     }
   }
-  return false;
+  for (HornRule &rule : rulesToRemove) { removeRule(rule); }
+  for (HornRule &rule : rulesToAdd) { addRule(rule); }
+  return changed;
 }
 
 HornClauseDB::horn_set_type HornClauseDB::m_empty_set;
