15650d5573ccdccc2a73167817efb76c085a756f
Joachim Bard <joachim.bard@cispa.de>
2022-07-29 13:15:41 +0200

    Fix table output of test script

--- spec_test/run-test.py
+++ spec_test/run-test.py
@@ -15,6 +15,7 @@ tmpdir = "tmp"
 texfilename = "table.tex"
 #testdirs = ["Kocher"]
 testdirs = ["openssl"]
+#testdirs = ["tmp/testdir"]
 #testdirs = ["Kocher", "openssl", "hacl-star"]
 iterations = 1
 test_placements = ["after-branch", "before-memory"]
@@ -81,10 +82,13 @@ def run_single_test(llfile, placement, choice):
 
 if sys.argv[1] == "--all":
     texfile = open("{}/{}".format(tmpdir, texfilename), "w")
-    print("\\begin{tabular}{l|cc|cc|cc}\n\\toprule", file=texfile)
+    print("\\begin{tabular}{l", end="", file=texfile)
+    for _ in test_placements:
+        print("|cc", end="", file=texfile)
+    print("}\n\\toprule", file=texfile)
     print("\\textbf{Benchmark}", end="", file=texfile)
     for placement in test_placements:
-        print("", "\multicolumn{2}{c|}{\\textbf{" + placement + "}}", sep=delim,
+        print("", "\multicolumn{2}{c}{\\textbf{" + placement + "}}", sep=delim,
                 end="", file=texfile)
     print("\\\\", file=texfile)
     for _ in test_placements:
