c83b09525a8cef50ab0658c3a81336af64afc855
Joachim Bard <joachim.bard@cispa.de>
2023-01-11 13:28:15 +0100

    Add Ed25519 sign benchmark

--- spec_test/hacl-star/hacl-aux.c
+++ spec_test/hacl-star/hacl-aux.c
@@ -5,8 +5,10 @@
 #include "hacl-aux.h"
 
 // print results (big file)
-//#define PRINT
+#define PRINT
 char output_filename[] = "ecdh-output.txt";
+
+#define MSG_SIZE (1024 * 1024 * 16)
 
 unsigned seed;
 double tstart = 0.0;
@@ -17,6 +19,28 @@ void init_seed(char** arg) {
 #ifdef PRINT
   printf("seed: 0x%x\n", seed);
 #endif
+}
+
+size_t get_length() {
+  return MSG_SIZE;
+}
+
+uint8_t *init_msg() {
+  uint8_t *msg = malloc(MSG_SIZE);
+  int *p = (int*) msg;
+  for (unsigned i = 0; i < MSG_SIZE / sizeof(int); ++i) {
+    // put "random" values into 'msg'
+    p[i] = rand();
+  }
+  return msg;
+}
+
+void init_key(uint8_t key[32]) {
+  int *p = (int*) key;
+  for (unsigned i = 0; i < 32 / sizeof(int); ++i) {
+    // put "random" values into 'a'
+    p[i] = rand();
+  }
 }
 
 void init(uint8_t a[32U]) {
@@ -39,6 +63,18 @@ void end_clock() {
   double tend = 1000.0 * ts.tv_sec + 1e-6 * ts.tv_nsec;
   // diff in ms
   fprintf(stderr, "  clock diff: %.1f\n", tend - tstart);
+}
+
+void display_signature(uint8_t signature[64]) {
+#ifdef PRINT
+  FILE *f = fopen(output_filename, "a");
+  fprintf(f, "encrypted: 0x%.2x", signature[0]);
+  for (unsigned i = 1; i < 64; ++i) {
+    fprintf(f, " %.2x", signature[i]);
+  }
+  fprintf(f, "\n");
+  fclose(f);
+#endif
 }
 
 void display(uint8_t out[32U]) {
