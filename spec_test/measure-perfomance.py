ae5517c54b2855b35e5d3c5b0c68e2ff0bc8b217
Joachim Bard <joachim.bard@cispa.de>
2023-01-31 12:18:12 +0100

    Improve performance measurement

--- spec_test/measure-perfomance.py
+++ spec_test/measure-perfomance.py
@@ -1,16 +1,34 @@
 import sys
 import os
 import subprocess
+import argparse
+import time
 
 timecmd = '/usr/bin/time'
 timeout = 30 # seconds
+cooloff = 60 # seconds
 delim = ','
 tmpdir = 'tmp'
 results_table_filename = 'perfomance.csv'
+default_iterations = 1
 
 seed_filename = "seeds.txt"
 runtime_prefix = "  runtime:"
 clock_diff_prefix = "  clock diff:"
+
+standard_benchmarks = [
+        'openssl/fixed/openssl-aes_cbc_encrypt_ct',
+        'openssl/fixed/openssl-aes_cbc_encrypt_non-ct',
+        'hacl-star/fixed/Hacl_Chacha20_encrypt',
+        'hacl-star/fixed/Hacl_Poly1305_32_mac'
+        ]
+
+all_testcase_suffixes = [
+        '_non-fixed',
+        '_after-branch_opt_fixed',
+        '_before-memory_opt_fixed'
+        ]
+
 
 def get_seeds():
     seeds = [];
@@ -38,7 +56,7 @@ def run_single_testcase(testcase, seeds):
             print("Timeout ({}s) expired for {}!".format(timeout, testcase), file=sys.stderr)
             return (-1, -1)
 
-#        print(p.stderr, end="", file=sys.stderr)
+        print(p.stderr, end="", file=sys.stderr)
         if p.stdout != "":
             print(p.stdout, file=sys.stdout)
         for line in p.stderr.splitlines():
@@ -52,7 +70,7 @@ def run_single_testcase(testcase, seeds):
     print("overall clock diff of {}: {:.2f}".format(testcase,  clock_diff))
     return (runtime, clock_diff)
 
-def run_benchmark(benchmark, iterations, seeds):
+def run_benchmark(benchmark, iterations, seeds, result_table):
     # check for 'lfence' in non-fixed version
     non_fixed = f'{benchmark}_non-fixed'
     disassemble = ['objdump', '-D', non_fixed]
@@ -60,34 +78,53 @@ def run_benchmark(benchmark, iterations, seeds):
     if 'lfence' in p.stdout:
         exit(f'Error: found "lfence" in {non_fixed} (which should not contain one)')
 
-    result_table = open(f'{tmpdir}/{results_table_filename}', 'w')
-    all_testcases = [
-            f'{benchmark}_non-fixed',
-            f'{benchmark}_after-branch_opt_fixed',
-            f'{benchmark}_before-memory_opt_fixed']
-
-    print('Benchmark', 'non-fixed', '', 'after-branch', '', 'before-memory', '',
-            sep=delim, file=result_table)
-    for _ in range(len(all_testcases)):
-        print('', 'runtime', 'clock diff', sep=delim, end='', file=result_table)
-    print(file=result_table)
-
     for _ in range(iterations):
         print(benchmark.split('/')[-1], end='', file=result_table)
-        for testcase in all_testcases:
-            (runtime, clock_diff) = run_single_testcase(testcase, seeds)
+        for suffix in all_testcase_suffixes:
+            if cooloff > 0:
+                time.sleep(cooloff)
+            (runtime, clock_diff) = run_single_testcase(benchmark + suffix, seeds)
             if runtime < 0 or clock_diff < 0:
 #                print('Error: negative runtime for', testcase, file=sys.stderr)
                 exit('Error: negative runtime for' + testcase)
-            print('', f'{runtime:.2f}', f'{clock_diff:.2f}', sep=delim, end='', file=result_table)
+#            print('', f'{runtime:.2f}', f'{clock_diff:.2f}', sep=delim, end='', file=result_table)
+            print('', f'{clock_diff:.2f}', sep=delim, end='', file=result_table)
         print(file=result_table)
 
-    result_table.close()
 
-if (len(sys.argv) < 2):
-    sys.exit("Script expects testfile as argument")
 
-seeds = get_seeds()
-#print("seeds:", seeds)
-# TODO: arg parser
-run_benchmark(sys.argv[1], 3, seeds)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--all', dest='all', default=False, action='store_true',
+            help='Run all standard benchmarks additionally to the explicit given ones')
+    parser.add_argument('benchmarks', nargs='*', help='Analyze all testcases of these benchmarks')
+    parser.add_argument('--single', dest='single', help='Run the single testcase')
+    parser.add_argument('-i', '--iterations', dest='iterations', default=default_iterations,
+            type=int, help='Specify how often a benchmark is tested')
+    args = parser.parse_args()
+
+    seeds = get_seeds()
+#    print("seeds:", seeds)
+    if args.single:
+        run_single_testcase(args.single, seeds)
+        return
+
+    test_benchmarks = args.benchmarks
+    if args.all:
+        test_benchmarks.extend(standard_benchmarks)
+
+    with open(f'{tmpdir}/{results_table_filename}', 'w') as result_table:
+#        print('Benchmark', 'non-fixed', '', 'after-branch', '', 'before-memory', '',
+        print('Benchmark', 'non-fixed', 'after-branch', 'before-memory',
+                sep=delim, file=result_table)
+        for _ in range(len(all_testcase_suffixes)):
+#            print('', 'runtime', 'clock', sep=delim, end='', file=result_table)
+            print('', 'clock', sep=delim, end='', file=result_table)
+        print(file=result_table)
+
+        for benchmark in test_benchmarks:
+            run_benchmark(benchmark, args.iterations, seeds, result_table)
+
+
+if __name__ == '__main__':
+    main()
